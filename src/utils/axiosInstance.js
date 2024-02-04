f73d6b8420a6b475b022bf9488f982626f317e13
Mahindar Leburu <mahindarleburu@gmail.com>
2024-02-05 01:14:31 +0800

    Prod deploy

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -7,7 +7,7 @@ export const instance = axios.create({
   // baseURL: "http://localhost:3001",
   // timeout: 1000,
   headers: { 
-    martech_token: "d96c0384e5dc5eec98e0a5febc93eaaeaf2b8049", 
+    martech_token: "e5246b1bdaa7c2b1cc3d8f0e65f4c7bec26c3936", 
     platform: 'tools' 
   },
 });
